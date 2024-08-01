d33460f9a81fc13c81890ff9b0806aa61e0c53f3
Kirill <syskirill.com>
2024-08-02 02:44:57 +0300

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Кирилл Сысоев on 2.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
