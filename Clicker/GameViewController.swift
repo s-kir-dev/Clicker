95533f008f945f4cadd5360d82be09861187b244
Kirill <syskirill.com>
2024-08-02 12:40:16 +0300

    timer was added

--- Clicker/GameViewController.swift
+++ Clicker/GameViewController.swift
@@ -13,8 +13,11 @@ class GameViewController: UIViewController {
     @IBOutlet weak var greenButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var currentScoreLabel: UILabel!
+    @IBOutlet weak var timeLeftLabel: UILabel!
     
     var currentScore : Int = 0
+    var seconds = 30
+    var timer : Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +26,40 @@ class GameViewController: UIViewController {
         for button in buttons {
             button?.addTarget(self, action: #selector(buttonTapped(_: )), for: .touchUpInside)
         }
-        
+        timeLeftLabel.text = "\(seconds)"
     }
     
     @objc func buttonTapped(_ sender: UIButton) {
+        makeTimer()
         currentScore += 1
-        updateLabel()
+        updateCounterLabel()
     }
     
-    func updateLabel() {
+    @objc func timerFired() {
+        if seconds > 0 {
+            seconds -= 1
+            updateTimeLeftLabel()
+        } else {
+            timer?.invalidate()
+            performSegue(withIdentifier: "GameOver", sender: self)
+        }
+    }
+    
+    func makeTimer() {
+        if let isTimer = timer {
+            return
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
+            debugPrint("Таймер запущен.")
+        }
+    }
+    
+    func updateCounterLabel() {
         currentScoreLabel.text = "Текущий счет : \(currentScore)"
+    }
+    
+    func updateTimeLeftLabel() {
+        timeLeftLabel.text = "\(seconds)"
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
